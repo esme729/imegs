90ef74ded9229b1a9814aa6f374655b8978b30b5
Esmay <Molinaesme729@gmail.com>
2022-08-30 10:21:22 -0600

    mvp

--- imegs/ViewController.swift
+++ imegs/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var spunky: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func slay(_ sender: Any) {
+        
+        if spunky.image == UIImage(named: "1")
+        {
+            spunky.image = UIImage(named: "2")
+        }
+        else
+        {spunky.image = UIImage(named: "1")
+        }
+    }
+    
 }
 
